90c64afecef2af2407eb70918bb22d824cf9368b
larissadcew <larisssa_barcellos@outlook.com>
2024-09-20 12:43:41 -0300

    add front

--- Movie_recomendation/Movie/urls.py
+++ Movie_recomendation/Movie/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+    path('Profile/', views.Profile, name='profile'),
+    path('Watchlist/', views.watchilist, name='Watchlist'),
+    path('Trending/', views.Trending, name='Trending'),
+
+]  
+
--- Movie_recomendation/Movie/views.py
+++ Movie_recomendation/Movie/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def watchilist(request):
+    return render(request , 'Watchlist.html')
+
+def Profile(request):
+    return render(request, 'Profile.html')
+
+def Trending(request):
+    return render(request, 'Trending.html')
